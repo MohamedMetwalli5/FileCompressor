24be0d00fdbf1d511d5889c6001cd9c6a864ab01
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-12-12 22:03:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,14 @@ using namespace std;
 std::string the_encoded_text;
 std::string the_decoded_text;
 
-struct Node
-{
+struct Node {
     char ch;
     int freq;
     Node *left, *right;
 };
 
 // Function to allocate a new tree node
-Node *makeNode(char ch, int freq, Node *left, Node *right)
-{
+Node *makeNode(char ch, int freq, Node *left, Node *right){
     Node *node = new Node();
 
     node->ch = ch;
@@ -31,25 +29,20 @@ Node *makeNode(char ch, int freq, Node *left, Node *right)
 }
 
 // Comparison object to be used to order the heap
-struct comp
-{
-    bool operator()(const Node *l, const Node *r) const
-    {
+struct comp{
+    bool operator()(const Node *l, const Node *r) const {
         // the highest priority item has the lowest frequency
         return l->freq > r->freq;
     }
 };
 
 // Traverse the Huffman Tree and store Huffman Codes in a map.
-void encode(Node *root, string str, unordered_map<char, string> &huffmanCode)
-{
-    if (root == nullptr)
-    {
+void encode(Node *root, string str, unordered_map<char, string> &huffmanCode) {
+    if (root == nullptr) {
         return;
     }
 
-    if (root->left == nullptr && root->right == nullptr)
-    {
+    if (root->left == nullptr && root->right == nullptr) {
         huffmanCode[root->ch] = (str != "") ? str : "1";
     }
     encode(root->left, str + "0", huffmanCode);  // all the left branches have a coding of "0"
@@ -57,42 +50,34 @@ void encode(Node *root, string str, unordered_map<char, string> &huffmanCode)
 }
 
 // Traverse the Huffman Tree and decode the encoded string
-void decode(Node *root, int &index, string str)
-{
+void decode(Node *root, int &index, string str) {
     if (root == nullptr)
     {
         return;
     }
 
-    if (root->left == nullptr && root->right == nullptr)
-    { // it is a leaf node
+    if (root->left == nullptr && root->right == nullptr) { // it is a leaf node
         the_decoded_text += root->ch;
         return;
     }
 
     index++;
 
-    if (str[index] == '0')
-    {                                   // we must move left
+    if (str[index] == '0') {                                   // we must move left
         decode(root->left, index, str); // we call it again on the left node
-    }
-    else
-    {                                    // we must move right
+    } else {                                    // we must move right
         decode(root->right, index, str); // we call it again on the right node
     }
 }
 
 // Builds Huffman Tree and decodes the given input text
-void buildHuffmanTree(string text)
-{
-    if (text == "")
-    { // there is no string to be decoded
+void buildHuffmanTree(string text) {
+    if (text == "") { // there is no string to be decoded
         return;
     }
 
     unordered_map<char, int> frequency_table;
-    for (char ch : text)
-    {
+    for (char ch : text) {
         frequency_table[ch]++;
     }
 
@@ -100,13 +85,11 @@ void buildHuffmanTree(string text)
     priority_queue<Node *, vector<Node *>, comp> pq;
 
     // creating a leaf node for each character and add it to the priority queue.
-    for (auto pair : frequency_table)
-    {
+    for (auto pair : frequency_table) {
         pq.push(makeNode(pair.first, pair.second, nullptr, nullptr)); //pair.first is the character to be encoded, and pair.second is the frequency of this character in the file
     }
 
-    while (pq.size() != 1)
-    {                          // while we can choose the two nodes with the lowest frequencies from the queue
+    while (pq.size() != 1) {                          // while we can choose the two nodes with the lowest frequencies from the queue
         Node *left = pq.top(); //creating the smallest frequency node and calling it left
         pq.pop();
         Node *right = pq.top(); //creating the second smallest frequency node and calling it right
@@ -123,51 +106,38 @@ void buildHuffmanTree(string text)
 
     cout << "Huffman Codes are:\n"
          << endl;
-    for (auto pair : huffmanCode)
-    {
+    for (auto pair : huffmanCode) {
         cout << pair.first << " | " << pair.second << endl;
     }
 
     // Print encoded string
     string str;
-    for (char ch : text)
-    {
+    for (char ch : text) {
         str += huffmanCode[ch];
     }
     the_encoded_text += str;
 
-    if (root->left == nullptr && root->right == nullptr)
-    { // if it is only one node and it is a leaf node in the tree
-        while (root->freq > 0)
-        {
+    if (root->left == nullptr && root->right == nullptr) { // if it is only one node and it is a leaf node in the tree
+        while (root->freq > 0) {
             the_decoded_text += root->ch;
         }
-    }
-    else
-    { // there is more than one node in the tree
+    } else { // there is more than one node in the tree
         int index = -1;
-        while (index < (int)str.size() - 1)
-        { // we didn't decode the whole encoded string
+        while (index < (int)str.size() - 1) { // we didn't decode the whole encoded string
             decode(root, index, str);
         }
     }
 }
 
-int main(int argc, char *argv[])
-{
-    if (!strcmp(argv[1], "") || !strcmp(argv[2], "") || !strcmp(argv[3], ""))
-    {
+int main(int argc, char *argv[]) {
+    if (!strcmp(argv[1], "") || !strcmp(argv[2], "") || !strcmp(argv[3], "")) {
         cout << "Please, enter a valid file path!" << endl;
         return 0;
-    }
-    else
-    {
+    }else {
         std::string line_;
         ifstream file_(argv[1]);
-        if (file_.is_open())
-        {
-            while (getline(file_, line_))
-            {
+        if (file_.is_open()) {
+            while (getline(file_, line_)) {
                 buildHuffmanTree(line_);
             }
 
